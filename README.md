75e25314f49835f6ff42cafe4ea3a61d18c1ce46
alex_p <aleksborislavskij@gmail.com>
2022-09-12 12:26:53 +0300

    test(index.test.js): add tests for comparing plain files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
+[![Maintainability](https://api.codeclimate.com/v1/badges/d14b1ec2c594b72e8471/maintainability)](https://codeclimate.com/github/Saimon398/frontend-project-lvl2/maintainability)
 [![Actions Status](https://github.com/Saimon398/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Saimon398/frontend-project-lvl2/actions)
---
+++ __tests__/index.test.js
@@ -0,0 +1,27 @@
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { readFileSync } from 'node:fs';
+import { test, expect, describe } from '@jest/globals';
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * @description Return absolute path to test files
+ * @param {String} filename Name of test file
+ * @returns {String} Absolute path
+ */
+const getFixturePath = (filename) => path
+  .join(__dirname, '..', '__fixtures__', filename);
+
+describe.each([
+  { file1: 'file1.json', file2: 'file2.json', expected: 'json-dif.txt' },
+])('.add($file1, $file2)', ({ file1, file2, expected }) => {
+  test('overall perfomance', () => {
+    const received1 = JSON.parse(readFileSync(getFixturePath(file1), 'utf-8'));
+    const received2 = JSON.parse(readFileSync(getFixturePath(file2), 'utf-8'));
+    expect(gendiff(received1, received2))
+      .toEqual(readFileSync(getFixturePath(expected)).toString());
+  });
+});
